4bc2f157a5fbb985b1147091a70458477017491f
Michal <michalmarchel62@gmail.com>
2021-01-14 13:28:40 +0100

    Functions, HTML and CSS created

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, c) {
+    if (c == "+") {
+         add(a, b);
+    }
+    if (c == "-") {
+        return substract(a, b);
+    }
+    if (c == "*") {
+        return multiply(a, b);
+    }
+    if (c == "/") {
+        return divide(a, b);
+    }
+}
