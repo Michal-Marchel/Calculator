031b40d6e1dd1e1c0b88ac32d774804262474e43
Michal <michalmarchel62@gmail.com>
2021-01-27 22:55:30 +0100

    function printSymbol created

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let input = document.getElementById("inputValue");
 let display = document.getElementById("topDiv");
 let op = identity;
+let decimalBtn = document.getElementById("decimalBtn");
+let symbol = document.getElementById("symbol");
+input.value = "";
 
 //Sum of two arguments.
 function add(a, b) {
@@ -21,8 +24,8 @@ function divide(a, b) {
 
 //Concat string
 function concatDigits(digito) {
-    if (!input.value) {
-        input.value = "";
+    if (input.value.length < 1 && digito === ".") {
+        input.innerHTML = input.value += "0";
         input.innerHTML = input.value += digito;
     } else {
         input.innerHTML = input.value += digito;
@@ -35,23 +38,23 @@ function clearInfo() {
     input.value = "";
     display.innerHTML = "";
     display.value = "";
+    decimalBtn.disabled = false;
+    printSymbol();
 }
 
-
-function sign {
-
-}
 
 function operate(operator) {
     if (input.value == "") {
         op = operator;
-    } else {
+    }  else {
         display.value = op(parseFloat(input.value), parseFloat(display.value));
         display.innerHTML = display.value;
         input.value = "";
         input.innerHTML = "";
     }
     op = operator;
+    decimalBtn.disabled = false; 
+    printSymbol(op);
 }
 
 function identity(x) {
@@ -60,15 +63,43 @@ function identity(x) {
 
 function result() {
     if (input.value == "") {
-        op = identity;
-        display.value = op(display.value);
-    } else {
+        display.value = op(parseFloat(display.value), parseFloat(display.value));
+        display.innerHTML = display.value;
+        
+    } else if (op == divide && display.value == 0 || op == divide && input.value == 0) {
+        display.innerHTML = "Is not possible to divide by 0";
+        input.value = "";
+        display.value = "";
+        input.innerHTML = "";
+    }else {
         display.value = op(parseFloat(input.value), parseFloat(display.value));
         display.innerHTML = display.value;
         input.value = "";
         input.innerHTML = "";
     }
+    decimalBtn.disabled = false;
+    printSymbol();
+}
 
+function printSymbol(operator) {
+    switch (operator) {
+        case add:
+            symbol.innerHTML = "+";
+        break;
+        case substract:
+            symbol.innerHTML = "-";
+        break;
+        case divide:
+            symbol.innerHTML = "/";
+        break;
+        case multiply:
+            symbol.innerHTML = "*";
+        break;
+        default:
+            symbol.innerHTML = "";
+    }
+
+}
     /*
     val1 = parseFloat(number2);
     // if second arg not provided getIdentity depending on the operation
@@ -83,4 +114,27 @@ function result() {
     number2 = "";
     inputValue.innerHTML = "";
     */
+
+
+function backspace() {
+    let check = false;
+    input.value = input.value.substring(0, input.value.length-1);
+    input.innerHTML = input.value;
+    for (let i = 0; i < input.value.length; i++) {
+        if (input.value[i] == ".") {
+            check = true;
+        }
+    }
+    if (check == false) {
+        decimalBtn.disabled = false;
+    }
+}
+
+function disableBtn() {
+    decimalBtn.disabled = true;
+}
+
+
+function sum (a, b) {
+    
 }
